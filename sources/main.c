66e297e5aa5663a4684275e1cd8fa9a764c26a3d
Eve Aviv Keinan <evavkein@gmail.com>
2025-05-30 11:14:03 +0300

    heredoc fixes and almost child signal fixes

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahavu <ahavu@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 08:27:53 by ekeinan           #+#    #+#             */
-/*   Updated: 2025/05/29 14:27:07 by ahavu            ###   ########.fr       */
+/*   Updated: 2025/05/30 11:10:32 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,14 @@ t_shell	*get_shell(void)
 void	init_shell(char **envp)
 {
 	ft_bzero(get_shell(), sizeof(t_shell));
-	init_signal_handlers();
+	sigaction(SIGINT,
+		&(struct sigaction){.sa_sigaction = sigint_handler},
+		&get_shell()->og_sigacts.sigint);
+	sigaction(SIGPIPE,
+		&(struct sigaction){.sa_sigaction = sigpipe_handler}, NULL);
+	sigaction(SIGQUIT,
+		&(struct sigaction){.sa_handler = SIG_IGN},
+		&get_shell()->og_sigacts.sigquit);
 	pid_to_buf(get_shell()->pid);
 	init_env(envp);
 }
